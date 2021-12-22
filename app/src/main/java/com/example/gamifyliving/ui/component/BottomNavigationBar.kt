a545837d2106a0b77f881f51562036c23f63c651
abhic <abhich1607@gmail.com>
2021-12-22 17:26:20 +0530

    Label added for nav bar icons

--- app/src/main/java/com/example/gamifyliving/ui/component/BottomNavigationBar.kt
+++ app/src/main/java/com/example/gamifyliving/ui/component/BottomNavigationBar.kt
@@ -1,12 +1,10 @@
 package com.example.gamifyliving.ui.component
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -25,7 +23,8 @@ fun BottomNavigationBar(
 
             items.forEachIndexed { _, screen ->
                     BottomNavigationItem(
-                        icon = { Icon(screen.icon!!, contentDescription = null) },
+                        icon = { Icon(screen.icon!!, contentDescription = stringResource(id = screen.resourceId!!)) },
+                        label = { Text(stringResource(id = screen.resourceId!!)) },
                         selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
                         onClick = {
                             navController.navigate(screen.route) {
