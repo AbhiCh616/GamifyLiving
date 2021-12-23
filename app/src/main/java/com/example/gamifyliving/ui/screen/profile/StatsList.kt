63b7cabd4dca88cc026559feda3821f88407ebd7
abhic <abhich1607@gmail.com>
2021-12-23 23:07:34 +0530

    Modifier moved to parent composable

--- app/src/main/java/com/example/gamifyliving/ui/screen/profile/StatsList.kt
+++ app/src/main/java/com/example/gamifyliving/ui/screen/profile/StatsList.kt
@@ -1,9 +1,13 @@
 package com.example.gamifyliving.ui.screen.profile
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gamifyliving.data.model.Stat
 import com.example.gamifyliving.ui.theme.GamifyLivingTheme
 
@@ -13,7 +17,10 @@ fun StatsList(
 ) {
     LazyColumn {
         items(stats) {
-            IndividualStat(it)
+            IndividualStat(
+                it,
+                Modifier.padding(vertical = 8.dp)
+            )
         }
     }
 }
