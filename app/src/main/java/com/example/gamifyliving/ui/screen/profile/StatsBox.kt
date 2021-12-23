7fac5aea40b0055c62ce586fa7d17243550203c1
abhic <abhich1607@gmail.com>
2021-12-23 23:23:48 +0530

    Preview for StatsBox added

--- app/src/main/java/com/example/gamifyliving/ui/screen/profile/StatsBox.kt
+++ app/src/main/java/com/example/gamifyliving/ui/screen/profile/StatsBox.kt
@@ -5,15 +5,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gamifyliving.R
 import com.example.gamifyliving.data.model.Stat
+import com.example.gamifyliving.ui.theme.GamifyLivingTheme
 
 @Composable
 fun StatsBox(
@@ -33,6 +36,20 @@ fun StatsBox(
         Spacer(modifier = Modifier.height(12.dp))
         TextButton(onClick = onViewAllClick) {
             Text(text = stringResource(id = R.string.view_all))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StatsBoxPreview() {
+    val stats = listOf(Stat("Health", 0.2F), Stat("Relationship", 0.75F))
+
+    GamifyLivingTheme {
+        Surface {
+            StatsBox(
+                stats = stats,
+            ) { }
         }
     }
 }
