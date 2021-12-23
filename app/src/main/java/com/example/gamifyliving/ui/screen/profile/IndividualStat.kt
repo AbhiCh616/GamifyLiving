08ac725232f2567fbf0b5409677521b07caaee50
abhic <abhich1607@gmail.com>
2021-12-23 19:29:09 +0530

    Code cleanup

--- app/src/main/java/com/example/gamifyliving/ui/screen/profile/IndividualStat.kt
+++ app/src/main/java/com/example/gamifyliving/ui/screen/profile/IndividualStat.kt
@@ -2,7 +2,9 @@ package com.example.gamifyliving.ui.screen.profile
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -10,7 +12,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gamifyliving.data.model.Stat
-
 
 @Composable
 fun IndividualStat(statDetails: Stat) {
@@ -20,18 +21,15 @@ fun IndividualStat(statDetails: Stat) {
             .fillMaxWidth()
             .padding(vertical = 8.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(statDetails.name)
-                IndividualStatBar(progress = statDetails.value * 100)
-            }
+            Text(statDetails.name)
+            IndividualStatBar(progress = statDetails.value * 100)
         }
     }
 }
