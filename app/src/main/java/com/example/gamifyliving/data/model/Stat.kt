fc4cdef09026b0abad65c355bb6f3053d10c05e1
abhic <abhich1607@gmail.com>
2022-01-05 13:13:37 +0530

    name is not longer Stat's primary key

--- app/src/main/java/com/example/gamifyliving/data/model/Stat.kt
+++ app/src/main/java/com/example/gamifyliving/data/model/Stat.kt
@@ -2,14 +2,18 @@ package com.example.gamifyliving.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity (indices = [Index(value = ["name"], unique = true)])
+
 data class Stat(
-    @PrimaryKey
     @ColumnInfo(name = "name")
     val name: String,
 
     @ColumnInfo(name = "value")
-    val value: Float
+    val value: Float,
+
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0
 )
