a14de55a406ba9d028e56ff59ee4b6afc82d0e0e
abhic <abhich1607@gmail.com>
2022-03-02 10:01:54 +0530

    Changing task status changes stat value

--- app/src/main/java/com/example/gamifyliving/domain/use_case/ChangeTaskStatus.kt
+++ app/src/main/java/com/example/gamifyliving/domain/use_case/ChangeTaskStatus.kt
@@ -1,14 +1,32 @@
 package com.example.gamifyliving.domain.use_case
 
 import com.example.gamifyliving.domain.model.Task
+import com.example.gamifyliving.domain.repository.RewardRepository
+import com.example.gamifyliving.domain.repository.StatRepository
 import com.example.gamifyliving.domain.repository.TaskRepository
+import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 class ChangeTaskStatus @Inject constructor(
-    private val repository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val rewardRepository: RewardRepository,
+    private val statRepository: StatRepository
 ) {
     suspend operator fun invoke(task: Task) {
         val newTask = task.copy(status = !task.status)
-        repository.updateTask(task = newTask)
+        taskRepository.updateTask(task = newTask)
+
+        rewardRepository.getRewardsForTask(task).collect { rewards ->
+            rewards.forEach { reward ->
+                val initialStat = statRepository.getStatById(reward.statId)!!
+                val finalStat =
+                    if (newTask.status) {
+                        initialStat.copy(value = initialStat.value + reward.points)
+                    } else {
+                        initialStat.copy(value = initialStat.value - reward.points)
+                    }
+                statRepository.updateStat(finalStat)
+            }
+        }
     }
 }
