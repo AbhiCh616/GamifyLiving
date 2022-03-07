6ade869d88ceef012a1754192a280d8fe333be36
abhic <abhich1607@gmail.com>
2022-03-07 10:48:21 +0530

    Sort completed tasks to end

--- app/src/main/java/com/example/gamifyliving/domain/use_case/GetTasks.kt
+++ app/src/main/java/com/example/gamifyliving/domain/use_case/GetTasks.kt
@@ -3,10 +3,15 @@ package com.example.gamifyliving.domain.use_case
 import com.example.gamifyliving.domain.model.Task
 import com.example.gamifyliving.domain.repository.TaskRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 class GetTasks @Inject constructor(
     private val repository: TaskRepository
 ) {
-    operator fun invoke(): Flow<List<Task>> = repository.observeTasks()
+    operator fun invoke(): Flow<List<Task>> = repository.observeTasks().transform {
+        emit(
+            it.sortedBy { task -> task.status }
+        )
+    }
 }
