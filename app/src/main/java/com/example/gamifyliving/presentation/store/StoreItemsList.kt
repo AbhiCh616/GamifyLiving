d6af7f0aceb2f1789e04c0e7b0742524e39208dc
abhic <abhich1607@gmail.com>
2022-03-16 11:39:01 +0530

    Store items list now has padding in-between

--- app/src/main/java/com/example/gamifyliving/presentation/store/StoreItemsList.kt
+++ app/src/main/java/com/example/gamifyliving/presentation/store/StoreItemsList.kt
@@ -1,8 +1,11 @@
 package com.example.gamifyliving.presentation.store
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.gamifyliving.domain.model.StoreItem
 
 @Composable
@@ -16,7 +19,8 @@ fun StoreItemsList(
             StoreItemCard(
                 storeItem = storeItem,
                 onCardClick = editStoreItem,
-                onBuyButtonClick = onBuyButtonClick
+                onBuyButtonClick = onBuyButtonClick,
+                modifier = Modifier.padding(vertical = 8.dp),
             )
         }
     }
