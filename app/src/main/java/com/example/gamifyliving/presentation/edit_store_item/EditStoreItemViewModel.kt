529904d5f5b7128144386fc8d59436d5fa76ce9c
abhic <abhich1607@gmail.com>
2022-03-17 15:00:57 +0530

    VM for editing a store item

---
+++ app/src/main/java/com/example/gamifyliving/presentation/edit_store_item/EditStoreItemViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.gamifyliving.presentation.edit_store_item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gamifyliving.domain.model.StoreItem
+import com.example.gamifyliving.domain.use_case.DeleteStoreItem
+import com.example.gamifyliving.domain.use_case.GetStoreItemById
+import com.example.gamifyliving.domain.use_case.UpdateStoreItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EditStoreItemViewModel @Inject constructor(
+    private val getStoreItemById: GetStoreItemById,
+    private val deleteStoreItem: DeleteStoreItem,
+    private val updateStoreItem: UpdateStoreItem,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    var name by mutableStateOf("")
+        private set
+
+    var costCoins by mutableStateOf("")
+        private set
+
+    private var selectedStoreItem: StoreItem? = null
+
+    init {
+        savedStateHandle.get<Int>("store_item_id")?.let { storeItemId ->
+            viewModelScope.launch {
+                getStoreItemById(id = storeItemId)?.let { storeItem ->
+                    selectedStoreItem = storeItem
+                    name = storeItem.name
+                    costCoins = storeItem.costCoins.toString()
+                }
+            }
+        }
+    }
+
+    fun onNameChange(newName: String) {
+        name = newName
+    }
+
+    fun onCostCoinsChange(newCostCoins: String) {
+        costCoins = newCostCoins
+    }
+
+    fun onDeleteClicked() = viewModelScope.launch {
+        selectedStoreItem?.let { deleteStoreItem(it) }
+    }
+
+    fun onSaveClicked() = viewModelScope.launch {
+        val updatedStoreItem = selectedStoreItem?.copy(name = name, costCoins = costCoins.toInt())
+        updatedStoreItem?.let { updateStoreItem(it) }
+    }
+
+}
