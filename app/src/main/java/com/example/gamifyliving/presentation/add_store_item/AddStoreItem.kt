35e89cc360f0c7fb9f349d31a65110ec65520144
abhic <abhich1607@gmail.com>
2022-03-17 06:44:55 +0530

    Add store item page added

---
+++ app/src/main/java/com/example/gamifyliving/presentation/add_store_item/AddStoreItem.kt
@@ -0,0 +1,15 @@
+package com.example.gamifyliving.presentation.add_store_item
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddStoreItem() {
+    Scaffold {
+        Surface(color = MaterialTheme.colors.background) {
+
+        }
+    }
+}
