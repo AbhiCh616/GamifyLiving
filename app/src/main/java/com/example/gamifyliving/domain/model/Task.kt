8b7ac8ad861a87ce61de593f79ccf47e98a56e3b
abhic <abhich1607@gmail.com>
2022-03-20 08:57:59 +0530

    date in domain model with default null value

--- app/src/main/java/com/example/gamifyliving/domain/model/Task.kt
+++ app/src/main/java/com/example/gamifyliving/domain/model/Task.kt
@@ -1,8 +1,11 @@
 package com.example.gamifyliving.domain.model
 
+import java.time.LocalDate
+
 data class Task(
     val name: String,
     val status: Boolean = false,
     val coinsReward: Int = 0,
-    val uid: Int = 0
+    val uid: Int = 0,
+    val date: LocalDate? = null,
 )
