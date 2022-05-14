c40b600904b7d7bd557cbd87772ff3967a268667
abhic <abhich1607@gmail.com>
2022-05-14 21:14:16 +0530

    GetTasks use-case now sort and filter the tasks

--- app/src/main/java/com/example/gamifyliving/domain/use_case/GetTasks.kt
+++ app/src/main/java/com/example/gamifyliving/domain/use_case/GetTasks.kt
@@ -1,7 +1,13 @@
 package com.example.gamifyliving.domain.use_case
 
+import com.example.gamifyliving.domain.model.Habit
 import com.example.gamifyliving.domain.model.Task
+import com.example.gamifyliving.domain.model.Todo
 import com.example.gamifyliving.domain.repository.TaskRepository
+import com.example.gamifyliving.domain.util.FilterTaskOn
+import com.example.gamifyliving.domain.util.SortCriteria
+import com.example.gamifyliving.domain.util.SortOrder
+import com.example.gamifyliving.domain.util.SortTasksBy
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
@@ -9,9 +15,36 @@ import javax.inject.Inject
 class GetTasks @Inject constructor(
     private val repository: TaskRepository
 ) {
-    operator fun invoke(): Flow<List<Task>> = repository.observeTasks().transform {
+    operator fun invoke(
+        sorts: List<SortTasksBy>? = null,
+        filters: Set<FilterTaskOn>? = null
+    ): Flow<List<Task>> = repository.observeTasks().transform { list ->
+
+        var updatedList = list
+
+        filters?.forEach { filter ->
+            updatedList = when (filter) {
+                FilterTaskOn.TODO -> updatedList.filterIsInstance<Todo>()
+                FilterTaskOn.HABIT -> updatedList.filterIsInstance<Habit>()
+                FilterTaskOn.WITH_TIME -> updatedList.filter { it.schedule?.timeSpan != null }
+            }
+        }
+
+        sorts?.forEach { sort ->
+            updatedList = when (sort.sortCriteria) {
+                SortCriteria.TIME -> updatedList.sortedBy { it.schedule?.timeSpan?.startTime }
+                SortCriteria.UNDONE -> updatedList.sortedBy { it.status }
+                SortCriteria.WITHOUT_TIME -> updatedList.sortedBy { it.schedule?.timeSpan == null }
+            }
+            if (sort.sortOrder == SortOrder.DESC) {
+                updatedList = updatedList.reversed()
+            }
+        }
+
         emit(
-            it.sortedBy { task -> task.status }
+            updatedList
         )
+
     }
+
 }
--- app/src/main/java/com/example/gamifyliving/presentation/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/gamifyliving/presentation/tasks/TasksViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.gamifyliving.domain.model.Task
 import com.example.gamifyliving.domain.use_case.ChangeTaskStatus
 import com.example.gamifyliving.domain.use_case.GetTasks
+import com.example.gamifyliving.domain.util.SortCriteria
+import com.example.gamifyliving.domain.util.SortTasksBy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,7 +18,9 @@ class TasksViewModel @Inject constructor(
     private val changeTaskStatus: ChangeTaskStatus
 ) : ViewModel() {
 
-    val tasks = getTasks()
+    val tasks = getTasks(
+        sorts = listOf(SortTasksBy(sortCriteria = SortCriteria.UNDONE))
+    )
 
     fun onCheckboxClicked(task: Task) = viewModelScope.launch {
         Log.d("VALE", task.toString())
