be5d4e8a5abcb0b848358bbf5f04aefe15653cd8
abhic <abhich1607@gmail.com>
2022-06-17 00:49:31 +0530

    Removed unused Task mapping code

--- app/src/main/java/com/example/gamifyliving/data/data_source/local/mapper/TaskMapper.kt
+++ app/src/main/java/com/example/gamifyliving/data/data_source/local/mapper/TaskMapper.kt
@@ -75,22 +75,6 @@ fun TimeSpanEntity.toTimeSpan() =
         endTime = endTime
     )
 
-
-fun Task.toDataModel() =
-    this.let {
-        val list = mutableListOf<RewardEntity>()
-        for (reward in this.rewards!!) {
-            list.add(
-                RewardEntity(
-                    taskId = this.id,
-                    statId = reward.statId,
-                    points = reward.points
-                )
-            )
-        }
-        list.toList()
-    }
-
 fun Reward.toDataModel(taskId: Int) =
     RewardEntity(
         taskId = taskId,
@@ -177,58 +161,3 @@ fun WeekDayScheduleEntity.toDomainModel() =
         saturday = saturday,
         timeSpan = timeSpan?.toTimeSpan()
     )
-
-fun TaskWithDetailsEntity.toDomainModel(): Task =
-    when {
-        this.todoWithSchedule != null -> Todo(
-            name = this.task.name,
-            coinsReward = todoWithSchedule.todo.coinsReward,
-            schedule = todoWithSchedule.todoSchedule?.let { todoScheduleEntity ->
-                DateSchedule(
-                    date = todoScheduleEntity.scheduledDate,
-                    timeSpan = todoScheduleEntity.timeSpan?.toTimeSpan()
-                )
-            },
-            status = this.task.status,
-            rewards = this.rewards?.toDomainModel(),
-            id = this.task.id
-        )
-        this.habitWithSchedule != null -> Habit(
-            name = this.task.name,
-            schedule =
-            when {
-                habitWithSchedule.everydaySchedule != null -> EverydaySchedule(
-                    timeSpan = habitWithSchedule.everydaySchedule.timeSpan?.toTimeSpan()
-                )
-                habitWithSchedule.repeatSchedule != null ->
-                    habitWithSchedule.repeatSchedule.let { repeatAfterScheduleEntity ->
-                        RepeatSchedule(
-                            startDate = repeatAfterScheduleEntity.startDate,
-                            interval = repeatAfterScheduleEntity.repeatAfter,
-                            timeSpan = repeatAfterScheduleEntity.timeSpan?.toTimeSpan()
-                        )
-                    }
-                habitWithSchedule.weekDaySchedule != null ->
-                    habitWithSchedule.weekDaySchedule.let { weekDayScheduleEntity ->
-                        WeekDaySchedule(
-                            sunday = weekDayScheduleEntity.sunday,
-                            monday = weekDayScheduleEntity.monday,
-                            tuesday = weekDayScheduleEntity.tuesday,
-                            wednesday = weekDayScheduleEntity.wednesday,
-                            thursday = weekDayScheduleEntity.thursday,
-                            friday = weekDayScheduleEntity.friday,
-                            saturday = weekDayScheduleEntity.saturday,
-                            timeSpan = weekDayScheduleEntity.timeSpan?.toTimeSpan()
-                        )
-                    }
-                else -> null
-            },
-            rewards = this.rewards?.toDomainModel(),
-            status = task.status,
-            id = task.id
-        )
-        else -> throw TypeCastException()
-    }
-
-fun List<TaskWithDetailsEntity>.toDomainModel() =
-    this.map { taskWithDetailsEntity -> taskWithDetailsEntity.toDomainModel() }
