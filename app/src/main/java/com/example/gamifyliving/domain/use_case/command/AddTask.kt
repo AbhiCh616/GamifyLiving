2a555a294c346abb7ca8f60c49d92d52f116ac86
abhic <abhich1607@gmail.com>
2022-06-26 20:25:50 +0530

    Coroutine management removed from use case

--- app/src/main/java/com/example/gamifyliving/domain/use_case/command/AddTask.kt
+++ app/src/main/java/com/example/gamifyliving/domain/use_case/command/AddTask.kt
@@ -1,32 +1,23 @@
 package com.example.gamifyliving.domain.use_case.command
 
-import com.example.gamifyliving.di.ApplicationScope
-import com.example.gamifyliving.di.DefaultDispatcher
 import com.example.gamifyliving.domain.model.entity.Habit
 import com.example.gamifyliving.domain.model.entity.Task
 import com.example.gamifyliving.domain.model.entity.Todo
 import com.example.gamifyliving.domain.repository.HabitRepository
 import com.example.gamifyliving.domain.repository.TodoRepository
 import com.example.gamifyliving.domain.util.runSuspendCatching
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AddTask @Inject constructor(
     private val todoRepository: TodoRepository,
-    private val habitRepository: HabitRepository,
-    @DefaultDispatcher private val dispatcher: CoroutineDispatcher,
-    @ApplicationScope private val scope: CoroutineScope,
+    private val habitRepository: HabitRepository
 ) {
     suspend operator fun invoke(task: Task) = runSuspendCatching {
-        scope.launch(dispatcher) {
-            if (task is Todo) {
-                todoRepository.add(task)
-            }
-            if (task is Habit) {
-                habitRepository.add(task)
-            }
+        if (task is Todo) {
+            todoRepository.add(task)
+        }
+        if (task is Habit) {
+            habitRepository.add(task)
         }
     }
 }
